f403569a6ce1e593f583d2024362277def52d157
Nicolas Collins <collins.nicolas@gmail.com>
2015-03-19 23:14:17 -0400

    Added base DB and updated API

--- PrimeAPI/Controllers/PrimeController.cs
+++ PrimeAPI/Controllers/PrimeController.cs
@@ -20,24 +20,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 
-namespace PrimeAPI
+
+namespace PrimeAPI.Controllers
 {
-    public static class WebApiConfig
+    public class PrimeController : ApiController
     {
-        public static void Register(HttpConfiguration config)
+        // GET api/prime
+        public string Get()
         {
-            // Web API configuration and services
-
-            // Web API routes
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            return "test";
         }
-    }
+
+        // POST api/prime
+        public string Post([FromBody]string value)
+        {
+            return "test";
+        }
+	}
 }
